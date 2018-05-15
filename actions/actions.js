ab4ec58b2c79dc056567e208be46f7c1cd9c88d7
alsuelo <carl.multimedia@gmail.com>
2018-05-14 19:48:00 -0500

    Remove unnecessary commented code

--- actions/actions.js
+++ actions/actions.js
@@ -1,17 +1,5 @@
 import { SET_NUMBER_OF_CONTACTS, SET_NUMBER_OF_LINES, SET_HOW_MUCH_DATA } from "../types/types";
 
-
-/*export const setMessage = (message) => {
-    return dispatch => {
-        dispatch({
-            type: SET_MESSAGE,
-            payload: {
-                message
-            }
-        })
-    }
-}
-*/
 export const setNumberOfContacts = text => ({
     type: SET_NUMBER_OF_CONTACTS,
     payload: text
